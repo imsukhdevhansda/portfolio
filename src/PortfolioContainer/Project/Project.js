75cfe91f235a4e7e1fcd054a3ae31d90b1b67c63
Sukhdev Hansda <sukhdevhansda8@gmail.com>
2022-12-19 14:44:06 +0530

    added tech stack

--- src/PortfolioContainer/Project/Project.js
+++ src/PortfolioContainer/Project/Project.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './Project.css';
 import myhoursProject from "../../assets/myhours.png";
 import nordstromProject from "../../assets/nordstrom.png";
+import zee5 from "../../assets/zee5.png";
+import shoppingbazzar from "../../assets/shoppingbazzar.png";
 import {AiOutlineGithub} from 'react-icons/ai';
 import {TbExternalLink} from 'react-icons/tb';
 import html5 from '../../assets/icons8-html-5-144.png';
@@ -13,6 +15,8 @@ import express from "../../assets/express.png";
 import mongodb from "../../assets/mongodb.png";
 import redux from "../../assets/redux.png";
 import github from "../../assets/github.png";
+import chakraui from "../../assets/chakraui.png";
+import firebase from "../../assets/firebase.png";
 
 export default function Project() {
     
@@ -45,6 +49,8 @@ export default function Project() {
               <div className="techStack">
                 <img src={react} />
                 <img src={redux} />
+                <img src={chakraui} />
+                <img src={css3} />
                 <img src={nodejs} />
                 <img src={express} />
                 <img src={mongodb} />
@@ -88,6 +94,7 @@ export default function Project() {
                 <img src={html5} />
                 <img src={css3} />
                 <img src={js} />
+                <img src={github} />
               </div>
               <div className="projectLinks">
                 <a
@@ -108,12 +115,84 @@ export default function Project() {
             </div>
           </div>
 
-          {/* <div className="project-card">
-            
-        </div>
-        <div className="project-card">
-            hello
-        </div> */}
+          <div className="project-card">
+            <div className="projectName">
+              <h3>Shopping Bazaar</h3>
+            </div>
+            <div className="projectImg">
+              <img src={shoppingbazzar} alt="" />
+            </div>
+            <div className="projectInfo">
+              <div className="contentProject">
+                About Shopping bazaar is a amazing Ecommerce website with the
+                sign in, signup, sorting and filtering functionality. Easily you
+                can find your favourite product and buy it. It was a
+                collaborative project. Cloned within a duration of 4 days.
+              </div>
+              <div className="techStack">
+                <img src={react} />
+                <img src={redux} />
+                <img src={chakraui} />
+                <img src={css3} />
+                <img src={firebase} />
+                <img src={github} />
+              </div>
+              <div className="projectLinks">
+                <a
+                  className="projectbtn1"
+                  href="https://github.com/imsukhdevhansda/shopping-bazaar"
+                  target="_blank"
+                >
+                  <AiOutlineGithub /> GitHub
+                </a>
+                <a
+                  className="projectbtn2"
+                  href="https://shoppingbazzar.vercel.app/"
+                  target="_blank"
+                >
+                  Demo <TbExternalLink />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="project-card">
+            <div className="projectName">
+              <h3>Zee5</h3>
+            </div>
+            <div className="projectImg">
+              <img src={zee5} alt="" />
+            </div>
+            <div className="projectInfo">
+              <div className="contentProject">
+                It was a collaborative project , we were 5 members in our group.
+                In this project we have clone the ZEE5.com. ZEE5 is a OTT
+                platform. where you can watch your favorite movies and shows.
+              </div>
+              <div className="techStack">
+                <img src={html5} />
+                <img src={css3} />
+                <img src={js} />
+                <img src={github} />
+              </div>
+              <div className="projectLinks">
+                <a
+                  className="projectbtn1"
+                  href="https://github.com/LaxmiWavale/ZEE5_clone"
+                  target="_blank"
+                >
+                  <AiOutlineGithub /> GitHub
+                </a>
+                <a
+                  className="projectbtn2"
+                  href="https://papaya-tarsier-523843.netlify.app/"
+                  target="_blank"
+                >
+                  Demo <TbExternalLink />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/PortfolioContainer/Skills/Skills.js
+++ src/PortfolioContainer/Skills/Skills.js
@@ -9,80 +9,149 @@ import GitHubCalendar from 'react-github-calendar';
 
 export default function Skills() {
   return (
-    <div className='skills-container' id='Skills'>
-        <div className='skills-header'>
-            <h3>Skills</h3>
-            <p>Hands On Experience</p>
-            <span className='header-underline'>
-                <span className='header-underline1'></span>
-            </span>
-        </div>
-        <div className="skillsDetails">
-            <div className="boxContainer">
-                <div className='skillBox skillbox1'>
-                    <div className='sillIcon'><ImHtmlFive color='red'/></div>
-                    <div className='skillTitle'>Web Technologies</div>
-                    <div className='skillInfo'>HTML, CSS, JavaScript</div>
-                </div>
-                <div className='skillBox skillbox2'>
-                    <div className='sillIcon'><GrJava /></div>
-                    <div className='skillTitle'>Web frameworks</div>
-                    <div className='skillInfo'>React,Redux</div>
-                </div>
-                <div className='skillBox skillbox3'>
-                    <div className='sillIcon'><FaJava color='blue'/></div>
-                    <div className='skillTitle'>CSS</div>
-                    <div className='skillInfo'>Material-UI,Styled-Components,Bootstrap</div> 
-                </div>
-                <div className='skillBox skillbox4'>
-                <div className='sillIcon'><GrNode/></div>
-                <div className='node'></div>
-                    <div className='skillTitle'>Node Js</div>
-                    <div className='skillInfo'>Backend</div> 
-                </div>
-                <div className='skillBox skillbox5'>
-                    <div className='sillIcon'><GiSkills color='rgb(109, 152, 0)'/></div>
-                    <div className='skillTitle'>Soft Skills</div>
-                    <div className='skillInfo'>Team work, Ready to learn new things, Communication Skills</div> 
-                </div>
-                <div className='skillBox skillbox6'>
-                    <div className='sillIcon'>
-                    <RiGitBranchLine color='blue'/>
-                    </div>
-                    <div className='skillTitle'>Code Version Control</div>
-                    <div className='skillInfo'>GitHub, Git</div>
-                </div>
-
-                <div className='skillBox skillbox7'>
-                    <div className='sillIcon'><img src='https://cdn-icons-png.flaticon.com/512/848/848525.png'></img></div>
-                    <div className='skillTitle'>Data Structure</div>
-                    <div className='skillInfo'>300+ Hours of DSA</div> 
-                </div>
-
-                <div className='skillBox skillbox8'>
-                    <div className='sillIcon'><img src="https://png.pngtree.com/png-vector/20190607/ourmid/pngtree-frontendinterfacemobilephonedeveloper-line-icon-png-image_1235458.jpg"></img></div>
-                    <div className='skillTitle'>Fronted Developer</div>
-                    <div className='skillInfo'>1200+ Hours of coding</div> 
-                </div>
-
-                <div className='skillBox skillbox9'>
-                    <div className='sillIcon'><img src="https://c3.klipartz.com/pngpicture/737/709/sticker-png-numix-circle-for-windows-mongodb-icon-thumbnail.png"></img></div>
-                    <div className='skillTitle'>Mongo DB</div>
-                    <div className='skillInfo'>Data Base</div> 
-                </div>
+    <div className="skills-container" id="Skills">
+      <div className="skills-header">
+        <h3>Skills</h3>
+        <p>Hands On Experience</p>
+        <span className="header-underline">
+          <span className="header-underline1"></span>
+        </span>
+      </div>
+      <div className="skillsDetails">
+        <div className="boxContainer">
+          <div className="skillBox skillbox1">
+            <div className="sillIcon">
+              <ImHtmlFive color="red" />
             </div>
-        </div>
-          
-             <div style={{display:"flex"}}>
-             <h1 className='calender_heading' >Github Calendar</h1>
+            <div className="skillTitle">Web Technologies</div>
+            <div className="skillInfo">HTML, CSS, JavaScript</div>
+          </div>
+          <div className="skillBox skillbox2">
+            <div className="sillIcon">
+              <GrJava />
             </div>
+            <div className="skillTitle">Web frameworks</div>
+            <div className="skillInfo">React,Redux</div>
+          </div>
+          <div className="skillBox skillbox3">
+            <div className="sillIcon">
+              <FaJava color="blue" />
+            </div>
+            <div className="skillTitle">CSS</div>
+            <div className="skillInfo">
+              Material-UI,Styled-Components,Bootstrap
+            </div>
+          </div>
+          <div className="skillBox skillbox4">
+            <div className="sillIcon">
+              <GrNode />
+            </div>
+            <div className="node"></div>
+            <div className="skillTitle">Node Js</div>
+            <div className="skillInfo">Backend</div>
+          </div>
+          <div className="skillBox skillbox5">
+            <div className="sillIcon">
+              <GiSkills color="rgb(109, 152, 0)" />
+            </div>
+            <div className="skillTitle">Soft Skills</div>
+            <div className="skillInfo">
+              Team work, Ready to learn new things, Communication Skills
+            </div>
+          </div>
+          <div className="skillBox skillbox6">
+            <div className="sillIcon">
+              <RiGitBranchLine color="blue" />
+            </div>
+            <div className="skillTitle">Code Version Control</div>
+            <div className="skillInfo">GitHub, Git</div>
+          </div>
 
+          <div className="skillBox skillbox7">
+            <div className="sillIcon">
+              <img src="https://cdn-icons-png.flaticon.com/512/848/848525.png"></img>
+            </div>
+            <div className="skillTitle">Data Structure</div>
+            <div className="skillInfo">300+ Hours of DSA</div>
+          </div>
 
-        <div className='calender'>
-        <GitHubCalendar username="imsukhdevhansda" style={{margin:"auto", padding:"20px"}} />
+          <div className="skillBox skillbox8">
+            <div className="sillIcon">
+              <img src="https://png.pngtree.com/png-vector/20190607/ourmid/pngtree-frontendinterfacemobilephonedeveloper-line-icon-png-image_1235458.jpg"></img>
+            </div>
+            <div className="skillTitle">Fronted Developer</div>
+            <div className="skillInfo">1200+ Hours of coding</div>
+          </div>
+
+          <div className="skillBox skillbox9">
+            <div className="sillIcon">
+              <img src="https://c3.klipartz.com/pngpicture/737/709/sticker-png-numix-circle-for-windows-mongodb-icon-thumbnail.png"></img>
+            </div>
+            <div className="skillTitle">Mongo DB</div>
+            <div className="skillInfo">Data Base</div>
+          </div>
         </div>
-       
-        
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <h1 className="calender_heading">Github Statistics</h1>
+      </div>
+
+      {/* <div
+        style={{ margin: "auto", width: "65%", marginTop: "6%", height: "30%" }}
+      >
+        <img
+          alt=" Sukhdev Hansda Activity Graph"
+          src="https://activity-graph.herokuapp.com/graph?username=imsukhdevhansda"
+          style={{ width: "100%" }}
+        />
+      </div> */}
+
+      {/* <div style={{ width: "30%", marginLeft: "20%" }}>
+        <img
+          align="center"
+          src="https://github-readme-stats.vercel.app/api/top-langs?username=omkundu&show_icons=true&locale=en&layout=compact"
+          alt="omkundu"
+          style={{ width: "100%" }}
+        />
+      </div> */}
+
+      <div style={{ width: "100%", marginLeft: "0%", display: "flex" }}>
+        <img
+          align="center"
+          src="https://github-readme-stats.vercel.app/api/top-langs?username=omkundu&show_icons=true&locale=en&layout=compact"
+          alt="omkundu"
+          style={{ width: "30%", marginLeft: "15%" }}
+        />
+        <img
+          align="center"
+          src="https://github-readme-stats.vercel.app/api?username=imsukhdevhansda&show_icons=true&locale=en&theme=tokyonight"
+          alt="imsukhdevhansda"
+          style={{ width: "30%", marginLeft: "7%" }}
+        />
+      </div>
+
+      <div style={{ width: "37%", marginLeft: "30%" }}>
+        <p align="center">
+          <img
+            align="center"
+            src="https://github-readme-streak-stats.herokuapp.com/?user=imsukhdevhansda&show_icons=true&locale=en&theme=tokyonight"
+            alt="imsukhdevhansda"
+            style={{ width: "100%"}}
+          />
+        </p>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <h1 className="calender_heading">Github Calendar</h1>
+      </div>
+
+      <div className="calender">
+        <GitHubCalendar
+          username="imsukhdevhansda"
+          style={{ margin: "auto", padding: "20px" }}
+        />
+      </div>
     </div>
-  )
+  );
 }
